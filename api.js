a36729d4704d385bb076e4f707b110829723a869
krishnaveni6286 <147615070+krishnaveni6286@users.noreply.github.com>
2024-06-14 11:42:43 +0530

    Update api.js

--- api.js
+++ api.js
@@ -24,10 +24,10 @@ export const url = {
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon){
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat, lon){
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
 
 
@@ -35,7 +35,7 @@ export const url = {
      * @param {string } query search query e.g.: "London", "New York"
     */ 
     geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 
 }
